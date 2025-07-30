19712d14385c713419292d0b228977c4c33a5521
pala_zz <palazzid88@gmail.com>
2025-07-30 01:31:01 -0300

    variables de entorno

--- sanity.config.js
+++ sanity.config.js
@@ -2,16 +2,17 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import 'dotenv/config'
+
 
 export default defineConfig({
   name: 'default',
   title: 'impulsar-turismo',
 
-  projectId: '6hgtd3uf',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
 
   plugins: [structureTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
